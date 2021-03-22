3b41f848d587a1587a6a55c1fc2a26b9431d8f55
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-22 19:18:56 +0500

    cssStyle

--- greeting-app/src/App.js
+++ greeting-app/src/App.js
@@ -7,15 +7,19 @@ let greeting="";
 let cssStyle={};
 if(CDate>=0 && CDate<12){
   greeting="Good Morning! Have a nice Day.";
+  cssStyle.color="green";
 }
 if(CDate>=12 && CDate<16){
   greeting="Good Afternoon! God bless you.";
+  cssStyle.color="yellow";
 }
 if(CDate>=16 && CDate<20){
   greeting="Good Evening! Hoping that you had a wonderful day.";
+  cssStyle.color="orange";
 }
 if(CDate>=20){
-  greeting="Hello jee, Good Morning!";
+  greeting="Good Night! Have some rest.";
+  cssStyle.color="blue";
 }
 function App() {
   return (
