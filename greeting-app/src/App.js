8a39fcca5ee8ba69486a0aec31bc94a177b7f03c
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-22 19:15:40 +0500

    updating greetings1

--- greeting-app/src/App.js
+++ greeting-app/src/App.js
@@ -4,8 +4,17 @@ import './App.css';
 let CDate=new Date()
 CDate=Cdate.getHours();
 let greeting="";
-let 
-if(CDate>0 && CDate<12){
+let cssStyle={};
+if(CDate>=0 && CDate<12){
+  greeting="Good Morning! Have a nice Day.";
+}
+if(CDate>=12 && CDate<16){
+  greeting="Good Afternoon! God bless you.";
+}
+if(CDate>=16 && CDate<20){
+  greeting="Good Evening! Hoping that you had a wonderful day.";
+}
+if(CDate>=20){
   greeting="Hello jee, Good Morning!";
 }
 function App() {
