cbb182aaf8b318789e2e8b587c8bcce7ba531db2
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-03-22 22:05:06 +0500

    Updated Background Images

--- greeting-app/src/App.js
+++ greeting-app/src/App.js
@@ -11,22 +11,22 @@ let cssStyle={
 if(CDate>=0 && CDate<12){
   greeting="Good Morning! Have a nice Day.";
   cssStyle.color="green";
-  cssStyle.backgroundImage='url("")';
+  cssStyle.backgroundImage='url("https://images.app.goo.gl/QNnmW6ArB3dyBpCt7")';
 }
 if(CDate>=12 && CDate<16){
   greeting="Good Afternoon! God bless you.";
   cssStyle.color="yellow";
-  cssStyle.backgroundImage='url("")';
+  cssStyle.backgroundImage='url("https://images.app.goo.gl/vdcFrjkvu1L7gW1x9")';
 }
 if(CDate>=16 && CDate<20){
   greeting="Good Evening! Hoping that you had a wonderful day.";
   cssStyle.color="orange";
-  cssStyle.backgroundImage='url("")';
+  cssStyle.backgroundImage='url("https://images.app.goo.gl/UdLo3jKyQrTjAyUC8")';
 }
 if(CDate>=20){
   greeting="Good Night! Have some rest.";
   cssStyle.color="blue";
-  cssStyle.backgroundImage='url("")';
+  cssStyle.backgroundImage='url("https://images.app.goo.gl/8mtY8yeRficSdBrSA")';
 }
 function App() {
   return (
